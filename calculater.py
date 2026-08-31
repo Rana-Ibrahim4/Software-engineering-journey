098b4107368c8a9751c9db38be81c44b1affa7e2
Rana Alshikhi <nano.212233@gmail.com>
2026-08-31 10:29:44 +0300

    Delete calculater.py

--- calculater.py
+++
@@ -1,8 +0,0 @@
-
-## A program calculates the numbers enterd by the user
-
-
-num1 = input("enter the first no:")
-num2 = input("enter the second no:")
-result = int (num1)+ int(num2)
-print(result)
