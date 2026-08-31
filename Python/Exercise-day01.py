310b862d7dd52c24d4fc8ad022427a2d48b94092
Rana Alshikhi <nano.212233@gmail.com>
2026-08-31 10:29:28 +0300

    Update and rename Exercise.py to Exercise-day01.py

--- Python/Exercise-day01.py
+++ Python/Exercise-day01.py
@@ -1,4 +1,4 @@
-## A program that asks the user about his name, age, study and city.
+##1 A program that asks the user about his name, age, study and city.
 
 
 name = input("what is your name?")
@@ -8,6 +8,12 @@ city = input("when is your live?")
 print("Hello " +  name +"!" + " and you are " +age+ "years old, " +"your study "+ study +"." + "and you live in "+city+"." )
 
 
+##2 A program calculates the numbers enterd by the user
 
+
+num1 = input("enter the first no:")
+num2 = input("enter the second no:")
+result = int (num1)+ int(num2)
+print(result)
 
 
