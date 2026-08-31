f024c7bfa6983d12b74e7417162bcf3a1591f479
Rana Alshikhi <nano.212233@gmail.com>
2026-08-31 10:25:12 +0300

    Update Day01-variables.py

--- Python/Day01-variables.py
+++ Python/Day01-variables.py
@@ -1,1 +1,16 @@
+#  Day01 - Python Basics
+Today I reviewed the basics of Python.
 
+## Topics 
+-Variables
+-Strings
+-Numbers
+-User Input
+-Basic Arithmatic Operations
+
+## Projects
+-First Python Program
+-Simple calculator
+
+## What I learned
+I learned how to store data in variables, work with strings and numbers,receive input from the user, and perform basic calculations.
