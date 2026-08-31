1a4877ad8cbea34553dee840ab3552523eaa3ebf
Rana Alshikhi <nano.212233@gmail.com>
2026-08-31 09:28:33 +0300

    Create first-program.py

---
+++ first-program.py
@@ -0,0 +1,9 @@
+
+## A program that asks the user about his name, age, study and city.
+
+
+name = input("what is your name?")
+age  = input("How old are you? ")
+study = input("what is your study?")
+city = input("when is your live?")
+print("Hello " +  name +"!" + " and you are " +age+ "years old, " +"your study "+ study +"." + "and you live in "+city+"." )
